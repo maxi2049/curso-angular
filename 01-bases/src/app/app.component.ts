cb5fdef65f87d45a6589a61b7a07ee364392b1ce
Maximiliano Iglesias <maxi2049@hotmail.com>
2021-03-04 13:58:02 -0300

    Primeros pasos

---
+++ 01-bases/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html' 
+})
+export class AppComponent {
+  titulo: string = 'Acumulador App';
+  numero: number = 10;
+  base: number = 5;
+
+  acumular(valor: number) {
+    this.numero += valor;
+  }
+}
