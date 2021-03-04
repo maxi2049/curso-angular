9d2cf04a3074a11db8c0b542cf017155ba555ee0
Maximiliano Iglesias <maxi2049@hotmail.com>
2021-03-04 14:28:02 -0300

    Modificación de path del acumulador

--- 01-bases/src/app/app.module.ts
+++ 01-bases/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { AcumuladorComponent } from './acumulador.component';
+import { AcumuladorComponent } from './acumulador/acumulador.component';
 
 @NgModule({
   declarations: [
