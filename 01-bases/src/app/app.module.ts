6371d20e29bf208bade4a553acfdb0be565e1b94
Maximiliano Iglesias <maxi2049@hotmail.com>
2021-03-05 12:49:01 -0300

    Creación del componente heroe

--- 01-bases/src/app/app.module.ts
+++ 01-bases/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AcumuladorComponent } from './acumulador/acumulador.component';
+import { HeroeComponent } from './heroes/heroe/heroe.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AcumuladorComponent
+    AcumuladorComponent,
+    HeroeComponent
   ],
   imports: [
     BrowserModule
