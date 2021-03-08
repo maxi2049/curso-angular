35ca27cc44f6356e550d624bd09e39798437ce26
Maximiliano Iglesias <maxi2049@hotmail.com>
2021-03-08 12:27:36 -0300

    modularización de componentes

--- 01-bases/src/app/app.module.ts
+++ 01-bases/src/app/app.module.ts
@@ -2,20 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { AcumuladorComponent } from './acumulador/acumulador.component';
-import { HeroeComponent } from './heroes/heroe/heroe.component';
-import { ListadoComponent } from './heroes/listado/listado.component';
+import { HeroesModule } from './heroes/heroes.module';
+import { AcumuladorModule } from './acumulador/acumulador.module';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    AcumuladorComponent,
-    HeroeComponent,
-    ListadoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroesModule,
+    AcumuladorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
