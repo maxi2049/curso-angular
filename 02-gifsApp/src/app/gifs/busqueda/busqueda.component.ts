7df39baf14dcfc53c9148272b32f27e55bae7258
Maximiliano Iglesias <maxi2049@hotmail.com>
2021-03-10 14:12:32 -0300

    Uso @ViewChild y variable local en html

--- 02-gifsApp/src/app/gifs/busqueda/busqueda.component.ts
+++ 02-gifsApp/src/app/gifs/busqueda/busqueda.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-busqueda',
   templateUrl: './busqueda.component.html',
   styleUrls: ['./busqueda.component.css']
 })
-export class BusquedaComponent implements OnInit {
+export class BusquedaComponent {
 
-  constructor() { }
 
-  ngOnInit(): void {
+  @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>;
+
+  buscar() {
+    const valor = this.txtBuscar.nativeElement.value
+
+    console.log( valor )
+
+    this.txtBuscar.nativeElement.value = '';
   }
 
 }
