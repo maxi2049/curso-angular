cc20bd267600bae6e449c6e4448169154059eccb
Maximiliano Iglesias <maxi2049@hotmail.com>
2021-03-12 14:09:44 -0300

    Estilos a la carga de gifs y depuracion de codigo

--- 02-gifsApp/src/app/gifs/services/gifs.service.ts
+++ 02-gifsApp/src/app/gifs/services/gifs.service.ts
@@ -47,7 +47,6 @@ export class GifsService {
 
     this.http.get<RespuestaBusquedaGIF>( `${ this.url }/search`, { params })
       .subscribe( (resp ) => {
-        console.log( resp.data );
         this.resultados = resp.data;
       
         localStorage.setItem( 'resultados', JSON.stringify( this.resultados ) )
